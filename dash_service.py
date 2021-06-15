ee9780af5fbd4dbff15aaa94dd7c81c6594e7105
Mika Kahola <mika.kahola@kolumbus.fi>
2021-06-15 14:00:13 +0300

    dash_service: Use nmap to search Garmin Virb
    Use nmap3 to search Garmin Virb ip address.
    
    Signed-off-by: Mika Kahola <mika.kahola@kolumbus.fi>

--- dash_service.py
+++ dash_service.py
@@ -9,25 +9,64 @@ import websockets
 import subprocess
 import configparser
 from pathlib import Path
+import nmap3
+import time
+
+from netifaces import interfaces, ifaddresses, AF_INET
 
 from virb import Virb
 
+use_virb = True
+
+def search_virb(my_device):
+    virb_ip = ""
+
+    def is_virb_ssid():
+        ssid = subprocess.check_output(['sudo', 'iwgetid']).decode("utf-8").split('"')[1]
+        if ssid == "VIRB-6267":
+            return True
+
+        return False
+
+    if is_virb_ssid():
+        return "192.168.0.1"
+
+    while virb_ip == "":
+        print("Dashboard searching Garmin Virb")
+
+        for ifaceName in interfaces():
+            addresses = [i['addr'] for i in ifaddresses(ifaceName).setdefault(AF_INET, [{'addr':'No IP addr'}] )]
+            if ifaceName == "wlan0":
+                my_ip = addresses[0].split('.')
+                break
+
+        my_ip[3] = "0/24"
+        ip = "."
+        ip = ip.join(my_ip)
+        nmap = nmap3.NmapHostDiscovery()
+        result=nmap.nmap_no_portscan(ip, "-sP")
+
+        for i in range(len(result)):
+             try:
+                device = result[list(result.keys())[i]]['hostname'][0]['name']
+                if device == my_device:
+                    virb_ip = list(result)[i]
+                    break
+             except:
+                    pass
+
+             time.sleep(1)
+
+    return virb_ip
+
 def run_dash_server():
     async def gps(websocket, path):
-        ssid = subprocess.check_output(['sudo', 'iwgetid']).decode("utf-8").split('"')[1]
-        conf_file = str(Path.home()) + "/.motorhome/network.conf"
-        config = configparser.ConfigParser()
-        config.read(conf_file)
+        global use_virb
 
-        try:
-            ip = config[ssid]['ip']
-        except:
-            print("no ip found, using default")
-            ip = "192.168.0.1"
-            pass
-        print("Virb ip: " + ip)
-
-        cam = Virb((ip, 80))
+        if use_virb:
+            ip = search_virb("Garmin-WiFi")
+            print("Virb ip: " + ip)
+            cam = Virb((ip, 80))
 
         while True:
             speed = '{:.0f}'.format(cam.get_speed())
