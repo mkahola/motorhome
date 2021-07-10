d51d55c2771f24f46d9bd4addc3569c5b85914f1
Mika Kahola <mika.kahola@gmail.com>
2021-07-10 23:00:43 +0300

    camerawindow: Add speed info on infobar
    Add speed info in km/h on infobar
    
    Signed-off-by: Mika Kahola <mika.kahola@gmail.com>

--- camerawindow.py
+++ camerawindow.py
@@ -145,6 +145,7 @@ class CameraWindow(QWidget):
         self.gps_disconnected = QPixmap(self.prefix + "no_gps.png").scaled(32, 32, Qt.KeepAspectRatio)
         self.gps_connected = QPixmap("")
         self.gpsInfoLabel = QLabel()
+        self.gpsSpeedLabel = QLabel("-- km/h")
 
         # Ruuvitag
         self.tempInfoLabel = QLabel()
@@ -161,6 +162,7 @@ class CameraWindow(QWidget):
         self.updateTime(datetime.now())
         self.updateTPMSWarn(infobar.tpmsWarn)
         self.updateGPSFix(infobar.gpsFix)
+        self.updateSpeed(infobar.speed)
         self.updateRecording(infobar.recording)
 
         hbox1 = QHBoxLayout()
@@ -168,6 +170,7 @@ class CameraWindow(QWidget):
         hbox1.addWidget(self.tempWarnLabel, alignment=Qt.AlignTop|Qt.AlignLeft)
         hbox1.addWidget(self.gpsInfoLabel, alignment=Qt.AlignTop|Qt.AlignLeft)
         hbox1.addWidget(self.recInfoLabel, alignment=Qt.AlignTop|Qt.AlignLeft)
+        hbox1.addWidget(self.gpsSpeedLabel, alignment=Qt.AlignTop|Qt.AlignRight)
         hbox1.addWidget(self.tempInfoLabel, alignment=Qt.AlignTop|Qt.AlignRight)
         hbox1.addWidget(self.timeLabel, alignment=Qt.AlignTop|Qt.AlignRight)
 
@@ -349,10 +352,16 @@ class CameraWindow(QWidget):
         self.updateTemperature(data['temperature'])
         self.updateTPMSWarn(data['tpms'])
         self.updateGPSFix(data['gpsFix'])
+        self.updateSpeed(data['speed'])
         self.updateRecording(data['recording'])
 
     def updateTime(self, t):
         self.timeLabel.setText("{:02d}".format(t.hour) + ":" + "{:02d}".format(t.minute))
+
+    def updateSpeed(self, speed):
+        if math.isnan(speed):
+            return
+        self.gpsSpeedLabel.setText("{:d}".format(round(3.6*speed)) + " km/h")
 
     def updateTemperature(self, temperature):
         if math.isnan(temperature):
