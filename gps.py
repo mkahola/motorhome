c18cf6d5131a48cba9d7a7d975dc730d26990142
Mika Kahola <mika.kahola@gmail.com>
2021-07-30 20:23:16 +0300

    gps: pylint fixes
    Fixes based on pylint static analysis tool (10/10)
    
    Signed-off-by: Mika Kahola <mika.kahola@gmail.com>

--- gps.py
+++ gps.py
@@ -1,30 +1,32 @@
-from PyQt5.QtGui import *
-from PyQt5.QtCore import *
-from PyQt5 import QtCore, QtGui
-
+"""
+GPS daemon thread
+"""
 import time
-import math
 import subprocess
 
 from datetime import datetime, timedelta
 from gps3.agps3threaded import AGPS3mechanism
 
-def update_datetime(dt, update):
-    if dt == "n/a" or update:
+from PyQt5.QtCore import pyqtSignal, QObject
+
+def update_datetime(date, update):
+    """ update date and time for system clock """
+    if date == "n/a" or update:
         return update
 
     print("GPS Thread: Update date and time")
     utc_offset = time.localtime().tm_gmtoff
-    my_time = datetime.strptime(dt, "%Y-%m-%dT%H:%M:%S.%fZ")
-    tz = timedelta(seconds=utc_offset)
-    my_time += tz
+    my_time = datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.%fZ")
+    timezone = timedelta(seconds=utc_offset)
+    my_time += timezone
 
-    t = my_time.strftime('{:}'.format(my_time.strftime('%Y-%m-%d %H:%M:%S')))
-    subprocess.call(['sudo', 'date', '-s', t])
+    cur_date = my_time.strftime('{:}'.format(my_time.strftime('%Y-%m-%d %H:%M:%S')))
+    subprocess.call(['sudo', 'date', '-s', cur_date])
 
     return True
 
 class Location(QObject):
+    """ location class """
     finished = pyqtSignal()
     exit_signal = pyqtSignal()
 
@@ -39,6 +41,7 @@ class Location(QObject):
         self.gps_thread.stream_data()
 
     def run(self):
+        """ Run gps sensor data receiver """
         self.gps_thread.run_thread()
         mode_prev = 0
         prev = 0
@@ -50,7 +53,8 @@ class Location(QObject):
                 prev = now
                 try:
                     if not time_updated:
-                        time_updated = update_datetime(self.gps_thread.data_stream.time, time_updated)
+                        time_updated = update_datetime(self.gps_thread.data_stream.time,
+                                                       time_updated)
 
                     mode = int(self.gps_thread.data_stream.mode)
                     if mode != mode_prev:
@@ -59,15 +63,15 @@ class Location(QObject):
 
                     # 2D or 3D fix required
                     if mode > 1:
-                        lat   = float(self.gps_thread.data_stream.lat)
-                        lon   = float(self.gps_thread.data_stream.lon)
-                        alt   = float(self.gps_thread.data_stream.alt)
+                        lat = float(self.gps_thread.data_stream.lat)
+                        lon = float(self.gps_thread.data_stream.lon)
+                        alt = float(self.gps_thread.data_stream.alt)
                         speed = float(self.gps_thread.data_stream.speed)
                         course = float(self.gps_thread.data_stream.track)
                         location = (lat, lon, alt, speed, course)
                         self.gpsLocation.emit(location)
-                except:
-                    pass
+                except ValueError:
+                    print("gps value error")
 
             time.sleep(0.1)
 
@@ -75,5 +79,6 @@ class Location(QObject):
         self.finished.emit()
 
     def stop(self):
+        """Stop running the gps thread"""
         print("GPS thread: received exit signal")
         self.running = False
