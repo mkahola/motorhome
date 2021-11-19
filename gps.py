1e4c22769da9e6c1460d3d17eb04c5eec86ae159
Mika Kahola <mika.kahola@gmail.com>
2021-11-19 17:48:57 +0200

    gps.py: Accept only GPS packets
    Accept only GPS packets i.e. with the id "gps".
    
    Signed-off-by: Mika Kahola <mika.kahola@gmail.com>

--- gps.py
+++ gps.py
@@ -28,14 +28,15 @@ class Location(QObject):
                 while self.running:
                     data = await websocket.recv()
                     data = json.loads(data)
-                    mode = data['mode']
-                    # emit GPS fix if changed
-                    if mode != mode_prev:
-                        self.gpsFix.emit(mode)
-                        mode_prev = mode
-                    if mode > 1:
-                        location = (data['lat'], data['lon'], data['alt'], data['speed'], data['course'], data['src'])
-                        self.gpsLocation.emit(location)
+                    if data['id'] == "gps":
+                        mode = data['mode']
+                        # emit GPS fix if changed
+                        if mode != mode_prev:
+                            self.gpsFix.emit(mode)
+                            mode_prev = mode
+                        if mode > 1:
+                            location = (data['lat'], data['lon'], data['alt'], data['speed'], data['course'], data['src'])
+                            self.gpsLocation.emit(location)
 
         asyncio.run(gps())
 
